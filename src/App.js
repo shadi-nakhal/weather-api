437066951fe2a676652c9949b19f6993967ebfcc
Manal-Jaber <manal.jaber94@gmail.com>
2020-10-15 22:27:53 +0300

    made it less spaced

--- src/App.js
+++ src/App.js
@@ -26,25 +26,18 @@ class App extends Component {
       city: "London",
       isLoaded: false,
       response: {}
-
     };
   }
-
- 
-
-
 
   handleInputChange = value => {
     this.setState({ city: value });
   };
-
 
   handleErrors = response => {
     if(!response.ok){
       this.setState({isLoaded: false})
     }
   }
-
 
   componentDidMount(){
     fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${this.state.city}&cnt=8&units=metric&appid=${api_key}`)
@@ -56,11 +49,9 @@ class App extends Component {
           })
         
         })
-        
   }
 
   searchCity = (citty) => {
-   
     fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${citty}&cnt=8&units=metric&appid=${api_key}`)
         .then(response => response.json())
         .then(json => {
@@ -69,9 +60,7 @@ class App extends Component {
             isLoaded: true,
             response: json
           })
-
         });
-        
   }
 
   getimg(id){
@@ -88,24 +77,73 @@ class App extends Component {
     }if(id >= 801 && id <= 805){
       return mostlycloudy
     } }
-
+  getBgColor(id){
+    if(id < 300){
+      return "#6d90aa";
+    }if (id >= 300 && id <= 499){
+      return "#6d90aa";
+    }if(id >= 500 && id <= 599){
+      return "#a2b4c2";
+    }if(id >= 600 && id <= 699){
+      return "#c8d8f0";
+    }if(id >= 700 && id <= 799){
+      return "#7ca4c2";
+    }if(id == 800){
+      return "#2590f2";
+    }if(id == 801){
+      return "#8cb9da";
+    }if(id > 801 && id <= 805){
+      return "#9ccef3";
+    } }
+  getSearchColor(id){
+    if(id < 300){
+      return "#526e99";
+    }if (id >= 300 && id <= 499){
+      return "#526e99";
+    }if(id >= 500 && id <= 599){
+      return "#7586b3";
+    }if(id >= 600 && id <= 699){
+      return "#acc4e9";
+    }if(id >= 700 && id <= 799){
+      return "#5e7eaf";
+    }if(id == 800){
+      return "#1d6cd2";
+    }if(id == 801){
+      return "#6a8ec5";
+    }if(id > 801 && id <= 805){
+      return "#769edb";
+    } }
+  getBtnColor(id){
+    if(id < 300){
+      return "#7d95a7";
+    }if (id >= 300 && id <= 499){
+      return "#3d548c";
+    }if(id >= 500 && id <= 599){
+      return "#5868a7";
+    }if(id >= 600 && id <= 699){
+      return "#58b1c8";
+    }if(id >= 700 && id <= 799){
+      return "#4660a0";
+    }if(id == 800){
+      return "#1a51bc";
+    }if(id == 801){
+      return "#4f6cb4";
+    }if(id > 801 && id <= 805){
+      return "#5879c8";
+    } }
 
   render() {
 
   var  data = api_data.list.slice(5,12).map(obj => {
       return obj
   })
-
-
-
     return (
-      <div className="app">
-        <Searchbox handleInput={this.handleInputChange} searchCity={this.searchCity}/>
+      <div className="app" backgroundColor={this.getBgColor}>
+        <Searchbox backgroundColor={this.getSearchColor} handleInput={this.handleInputChange} searchCity={this.searchCity}/>
         <Currentweather isLoaded={this.state.isLoaded} data={this.state.response} getimg={this.getimg} />
         <Hourlyweather isLoaded={this.state.isLoaded} data={this.state.response} getimg={this.getimg}/>
       </div>
     );
   }
 }
-
 export default App;
--- src/components/Hourlyweather.js
+++ src/components/Hourlyweather.js
@@ -1,15 +1,11 @@
 import React from "react";
 import "./Hourlyweather.css";
 
-
-
 class Hourlyweather extends React.Component {
   
     state = {
       input: ""
     };
-
-
     render() {
       let dataprop = this.props.data ;
       let isLoaded = this.props.isLoaded;
@@ -30,33 +26,16 @@ class Hourlyweather extends React.Component {
         </span>
       }
       else{
-
-  
-
-        // let date = new Date().getHours();
-        // let dattta = dataprop.list.forEach((element, index) => {
-        //   let temp = new Date(element.dt_txt).getHours() - date;
-        //   console.log(temp, "ss")
-        //   if(temp <= 1){
-        //     dataprop.list.splice(index,1);
-        //   }
-        // });
-
-       //console.log(dataprop.list)
       return (
         <section className="hourlyweather">
           <ul className="hourlyweather__timelist">
            {dataprop.list.map((item, key)=> 
-                
-               <li key={key} className="hourlyweather__timelist--item"><time>{new Date(item.dt_txt).getHours().toString().padStart(2, '0')+":00"}</time><img src={this.props.getimg(item.weather[0].id)} alt={this.props.getimg(item.weather[0].main.description)}/>{Math.round(item.main.temp)}&#176;C</li>
+              <li key={key} className="hourlyweather__timelist--item"><time>{new Date(item.dt_txt).getHours().toString().padStart(2, '0')+":00"}</time><img src={this.props.getimg(item.weather[0].id)} alt={this.props.getimg(item.weather[0].main.description)}/>{Math.round(item.main.temp)}&#176;C</li>
            )}
-           
-              </ul>
+          </ul>
         </section>
       );
-            }  
-  }
-
+      }  
+    }
 }
-
   export default Hourlyweather;
